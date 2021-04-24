fc1e866d7f02d448f4cc9b36ab7371b36303f238
frank <frankjmclarke@github.com>
2021-04-23 20:27:35 -0700

    Removes unused interfaces

--- app/src/main/java/com/fclarke/mvc_mvp_mvvm/model/CountryInterface.kt
+++ app/src/main/java/com/fclarke/mvc_mvp_mvvm/model/CountryInterface.kt
@@ -6,23 +6,7 @@ import com.google.gson.annotations.SerializedName
 import retrofit2.Call
 import retrofit2.http.GET
 
-interface CountryInterface2 {
-    @GET("/rest/v2/all")
-    fun getCountries(): Call<List<Country>>
-}
-
 class CountryInterface {
     @SerializedName("name")
     var countryName: String? = null
-}
-interface superheroAPIold {
-    @GET("marvel") // making get request at marvel end-point
-    fun getHeroes(): Call<List<Hero?>?>?
-}
-
-interface ApiService {
-
-    @GET("users")
-    fun getUsers(): Call<MutableList<Country>>
-
 }
